e7e13cf499e7e8cbd2e17df9383add0fc13cabfe
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-04-22 15:11:26 -0300

    Create home.dart
    v001

---
+++ home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? Key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        child: Text('This is home screen.')
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Icon(Icons.menu,
+        color: tdBlack,
+        size: 30,
+        ),
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child:  Image.asset('assets/usericon.png')),
+        )
+      ]),
+    );
+  }
+}
