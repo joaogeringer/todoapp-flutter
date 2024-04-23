211402eae4c3d6c781759a5b70c47ba1e6c26e69
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-04-23 14:53:12 -0300

    Create todo_item.dart
    v001

---
+++ todo_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ToDoItem extends StatelessWidget {
+  const ToDoItem( {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('ToDo Item'),
+    );
+  }
+}
