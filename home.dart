4ae50d958b869c630f4df6d3ecae5a7a8d3eca24
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-04-25 15:40:33 -0300

    Update home.dart
    v005

--- home.dart
+++ home.dart
@@ -1,47 +1,105 @@
 import 'package:flutter/material.dart';
 import '../constants/colors.dart';
+import '../model/todo.dart';
 import '../widgets/todo_item.dart';
 
 
 class Home extends StatelessWidget {
-  const Home(Key? key) : super (key: key);
+ Home(Key? key) : super (key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    final todosList = ToDo.todoList();
+
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20, 
-          vertical: 15
-        ),
-        child: Column(
-          children: [
-            searchBox()
-          Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 50, bottom: 20),
-                    child: Text('All ToDos', style: TextStyle(
-                      fontSize: 30, 
-                      fontWeight: FontWeight.w500),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20, 
+              vertical: 15
+            ),
+            child: Column(
+              children: [
+                searchBox()
+              Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text('All ToDos', style: TextStyle(
+                          fontSize: 30, 
+                          fontWeight: FontWeight.w500),
+                        ),
+                      ),
+
+                      for ( ToDo todoo in todosList  )
+                      ToDoItem(todo: todoo,),           
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                    bottom: 20, 
+                    right: 20, 
+                    left: 20
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [BoxShadow(
+                      color: Colors.grey, 
+                      offset: Offset(0.0 , 0.0,)
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Add new ToDo item',
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
-                  ToDoItem()
-                ],
-              ),
-            )
-          ],
-        ),
+                )
+                Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                  ),
+                  child: ElevatedButton(
+                    child: Text('+', style: TextStyle(fontSize: 40,),),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: tdBlue,
+                      minimumSize: Size(60, 60),
+                      elevation: 10,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
 
   Widget searchBox() {
     return Container(
-              padding: EdgeInsets.symmetric(horizontal: 15),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20)
