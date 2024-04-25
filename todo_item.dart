bd47cdcd8510ebb50654c0d6a49654c46713e6e4
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-04-25 15:40:54 -0300

    Update todo_item.dart
    v004

--- todo_item.dart
+++ todo_item.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 import '../constants/colors.dart';
+import '../model/todo.dart';
 
 class ToDoItem extends StatelessWidget {
-  const ToDoItem( {Key? key}) : super(key: key);
+  final ToDo todo;
+
+  const ToDoItem( {Key? key, required this.todo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(bottom: 20),
       child: ListTile(
         onTap: () {
           print('Clicked on todo item')
@@ -16,11 +20,14 @@ class ToDoItem extends StatelessWidget {
         ),
         contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
-        leading: Icon(Icons.check_box, color: tdBlue,),
-        title: Text('Check Mail', 
+        leading: Icon(
+        todo.isDone ? Icons.check_box: Icons.check_box_outline_blank,
+        color: tdBlue,),
+        title: Text(
+        todo.todoText!, 
         style: TextStyle(fontSize: 16, 
         color: tdBlack,
-        decoration: TextDecoration.lineThrough,
+        decoration: todo.isDone? TextDecoration.lineThrough: null,
             ),
          ),
          trailing: Container (
