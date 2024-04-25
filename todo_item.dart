bb39a08c332db3094bb722ff04ed675f0777cf1e
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-04-25 14:46:29 -0300

    Update todo_item.dart
    v002

--- todo_item.dart
+++ todo_item.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
+import '../constants/colors.dart';
+
 class ToDoItem extends StatelessWidget {
   const ToDoItem( {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('ToDo Item'),
+      child: ListTile(
+        onTap: () {
+          print('Clicked on todo item')
+        },
+        shape: RoundedRectangleBorder(borderRadius:  BorderRadius.circular(20)
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(Icons.check_box, color: tdBlue,),
+        title: Text('Check Mail', 
+        style: TextStyle(fontSize: 16, 
+        color: tdBlack,
+        decoration: TextDecoration.lineThrough,
+            ),
+         ),
+         trailing: Container (
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              print('Delete Button')
+            },
+          ),
+         ),
+      ),
     );
   }
 }
